69aadf2e67f587cc38fd74e1fb5913fe550710db
Mikita Kukavenka <nkukovenko@by.byelex.com>
2018-12-19 17:48:06 +0300

    Fixes in view

---
+++ app/src/components/components.module.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const appComponents = angular.module('app.components', []);
+
+import PageHeaderComponent from './pageheader.component.js';
+
+appComponents.component('pageheader', PageHeaderComponent);
