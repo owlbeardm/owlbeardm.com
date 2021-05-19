17bd0c3197c82c3c15ad71bc41ac7ae9e25125b0
Mikita Kukavenka <godinfrog@gmail.com>
2021-05-19 18:00:09 +0300

    fixed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,39 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = merge(common, {
   output: {
-    filename: '[name].[hash].js',
+    filename: "[name].[hash].js",
   },
   optimization: {
-    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})]
-
-  }
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.((s[ac])|c)ss$/,
+          chunks: "all",
+          enforce: true,
+          // maxSize: 244000
+        },
+        res: {
+          name: "res",
+          test: /\.json/,
+          chunks: 'all',
+          enforce: true,
+          maxSize: 244000,
+        },
+        vendor: {
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          // chunks: 'all',
+          enforce: true,
+          maxSize: 244000,
+        },
+      },
+    },
+  },
 });
